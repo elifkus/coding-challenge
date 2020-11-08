3bdeca7b82700ebaef8b3d7d4d1353b29d1d5728
Elif T. Kuş <elifkus@gmail.com>
2020-11-08 16:43:07 +0300

    Implemented endpoint / that returns health status in Twitter.API

--- tweet-api/spago.dhall
+++ tweet-api/spago.dhall
@@ -3,7 +3,14 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "psci-support" ]
+, dependencies = [ "aff"
+                  , "aff-promise"
+                  , "console"
+                  , "effect"
+                  , "express"
+                  , "psci-support"
+                  , "simple-json"
+                  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- tweet-api/src/Main.purs
+++ tweet-api/src/Main.purs
@@ -1,10 +1,28 @@
 module Main where
 
-import Prelude
 
+import Data.Maybe (maybe)
+import Data.Nullable (toMaybe)
 import Effect (Effect)
 import Effect.Console (log)
+import Env (hostEnv, portEnv)
+import Node.Express.App (App, listenHostHttp, get)
+import Node.Express.Handler (Handler)
+import Node.Express.Response (send)
+import Node.HTTP (Server)
+import Prelude (bind, identity, show, ($), (<>),(<#>))
 
-main :: Effect Unit
+
+health :: Handler
+health = send "{\"status\":\"ok\"}"
+
+app :: App
+app = do
+  get "/" health
+
+main :: Effect Server
 main = do
-  log "🍝"
+  host <- hostEnv <#> toMaybe <#> maybe "0.0.0.0" identity
+  port <- portEnv <#> toMaybe <#> maybe 9000 identity
+  listenHostHttp app port host \_ ->
+    log $ "Listening on http://" <> host <> ":" <> show port
