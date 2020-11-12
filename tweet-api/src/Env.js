f5fc20eab212cedd917d774758e95ae6fe7c5f05
Elif T. Kuş <elifkus@gmail.com>
2020-11-13 00:38:19 +0300

    Deleted purescript attempt

--- tweet-api/src/Env.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-
-exports.hostEnv = () => process.env.HOST
-exports.portEnv = () => process.env.PORT
--- tweet-api/src/Env.purs
+++
@@ -1,8 +0,0 @@
-module Env where
-
-import Data.Nullable (Nullable)
-import Effect (Effect)
-
-foreign import hostEnv :: Effect (Nullable String)
-
-foreign import portEnv :: Effect (Nullable Int)
