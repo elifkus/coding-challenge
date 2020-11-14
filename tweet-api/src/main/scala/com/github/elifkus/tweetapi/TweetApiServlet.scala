97d1a9306a94b023932214b7dc034d9f58c0d32d
Elif T. Kus <elifkus@gmail.com>
2020-11-14 17:02:49 +0300

    Called SentimentAnalysis service

--- tweet-api/src/main/scala/com/github/elifkus/tweetapi/TweetApiServlet.scala
+++ tweet-api/src/main/scala/com/github/elifkus/tweetapi/TweetApiServlet.scala
@@ -4,6 +4,8 @@ import com.github.elifkus.tweetapi.models.Analysis
 import org.scalatra._
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.json._
+import sttp.client3._
+
 
 class TweetApiServlet extends ScalatraServlet with JacksonJsonSupport {
   protected implicit val jsonFormats: Formats = DefaultFormats
@@ -13,7 +15,17 @@ class TweetApiServlet extends ScalatraServlet with JacksonJsonSupport {
   }
 
   get("/api/analysis") {
-    AnalysisData.oneAnalysis
+    println("/api/analysis called")
+    val signup = Some("yes")
+
+    val request = basicRequest.post(uri"http://localhost:9000/toxicity")
+      .body("""{"sentences": ["This is a bad sentence!"],"threshold": 0.9}""")
+      .contentType("application/json")
+
+    val backend = HttpURLConnectionBackend()
+    val response = request.send(backend)
+
+    response.body
   }
 
   before() {
@@ -22,11 +34,3 @@ class TweetApiServlet extends ScalatraServlet with JacksonJsonSupport {
 
 }
 
-
-object AnalysisData {
-
-  /**
-   * Some fake Analysis data so we can simulate retrievals.
-   */
-  var oneAnalysis = Analysis(true, true, true, true, true, true, true)
-}
