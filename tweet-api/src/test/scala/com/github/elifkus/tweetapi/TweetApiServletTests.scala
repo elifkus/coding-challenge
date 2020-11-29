4c62e45d320d93c0042e1568513baf80d3372ba5
Elif T. Kus <elifkus@gmail.com>
2020-11-29 20:43:14 +0300

    Added test in TweetApi

--- tweet-api/src/test/scala/com/github/elifkus/tweetapi/TweetApiServletTests.scala
+++ tweet-api/src/test/scala/com/github/elifkus/tweetapi/TweetApiServletTests.scala
@@ -1,6 +1,8 @@
 package com.github.elifkus.tweetapi
 
 import org.scalatra.test.scalatest._
+import com.github.elifkus.tweetapi.TweetApiServlet
+import com.github.elifkus.tweetapi.models._
 
 class TweetApiServletTests extends ScalatraFunSuite {
 
@@ -12,4 +14,59 @@ class TweetApiServletTests extends ScalatraFunSuite {
     }
   }
 
+  test("extractApiOutput extracts content") {
+    val identityAttack = Sentiment(true, 0.98, 0.01)
+    val insult = Sentiment(false, 0.50, 0.01)
+    val obscene = Sentiment(true, 0.98, 0.01)
+    val severeToxicity = Sentiment(false, 0.50, 0.01)
+    val sexualExplicit = Sentiment(true, 0.98, 0.01)
+    val threat = Sentiment(true, 0.98, 0.01)
+    val toxicity = Sentiment(true, 0.98, 0.01)
+
+    val sentimentReport = SentimentReport(identityAttack,
+                                              insult,
+                                              obscene,
+                                              severeToxicity,
+                                              sexualExplicit,
+                                              threat,
+                                              toxicity)
+    
+    val sentimentAnalysisOutput = SentimentAnalysisOutput("Horrible sentence", sentimentReport)
+    val resultList = List(sentimentAnalysisOutput)
+
+    val tweetInput = TweetInput("Horrible sentence")
+    val servlet = new TweetApiServlet()
+    val analysis = servlet.extractApiOutput(tweetInput, resultList)
+
+    assert(obscene.sentimentmatch === analysis.obscene)
+    assert(insult.sentimentmatch === analysis.insult)
+  }
+
+  /*test("extractApiOutput extracts content when one field misses") {
+    val identityAttack = Sentiment(true, 0.98, 0.01)
+    val obscene = Sentiment(true, 0.98, 0.01)
+    val severeToxicity = Sentiment(false, 0.50, 0.01)
+    val sexualExplicit = Sentiment(true, 0.98, 0.01)
+    val threat = Sentiment(true, 0.98, 0.01)
+    val toxicity = Sentiment(true, 0.98, 0.01)
+
+    val sentimentReport = SentimentReport(Some(identityAttack),
+                                              None,
+                                              Some(obscene),
+                                              Some(severeToxicity),
+                                              Some(sexualExplicit),
+                                              Some(threat),
+                                              Some(toxicity))
+    
+    val sentimentAnalysisOutput = SentimentAnalysisOutput("Horrible sentence", sentimentReport)
+    val resultList = List(sentimentAnalysisOutput)
+
+    val tweetInput = TweetInput("Horrible sentence")
+    val servlet = new TweetApiServlet()
+    val analysis = servlet.extractApiOutput(tweetInput, resultList)
+
+    assert(obscene.sentimentmatch === analysis.obscene)
+
+  }
+*/
 }
